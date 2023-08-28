89166f5b5251cc4b825920551bf26b51830c44e4
manisha <manishab034@gmail.com>
2023-08-28 14:15:19 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -10,52 +10,7 @@ import MovieList from './Components/MovieList';
 
 
 function App() {
-
-  const [movies, setMovies]=useState([])
-  const navigate = useNavigate()
-  const favorites = useSelector((state) => state.favorites);
-
-  const dispatch = useDispatch(); 
-
-  const moviesList ='https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=edca993d7c8e2287af01d26d7b0b043e&page=1'
-
-  const image_path='https://image.tmdb.org/t/p/w1280'
-
-  const searchList ='https://api.themoviedb.org/3/search/movie?sort_by=popularity.desc&api_key=edca993d7c8e2287af01d26d7b0b043e&query='
-
-  useEffect(()=>{
-    
-      fetch(moviesList)
-      .then(res=>res.json()).then(data=>{
-       setMovies(data.results)
-      })
-    }
-
-  ,[])
-
-  const handleAddToFavorites = (movie) => {
-    dispatch(addToFavorites(movie));
-  };
-
-  const handleRemoveFromFavorites = (movieID) => {
-    dispatch(removeFromFavorites(movieID));
-  };
-
-
-  const handleChange = (val)=>{
-    fetch(`${searchList}`+val)
-      .then(res=>res.json()).then(data=>{
-       setMovies(data.results)
-      })
-  }
-
-  const [favourites, setFavourites] = useState([])
-
-  const addFavMovie =(movie)=>{
-    const newFavList=[...favourites,movie]
-    console.log(newFavList)
-  }
-
+  
   return (
     <div className="App">
     
